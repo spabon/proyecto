cab7af592a25b0c257f99720c76dfee78ce23c51
spabon <sspabonee@hotmail.com>
2015-04-29 03:40:28 +0000

    Generate scaffold for User and Post change root for post's index

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150427220756) do
+ActiveRecord::Schema.define(version: 20150429024935) do
 
   create_table "articles", force: true do |t|
     t.string   "title"
     t.text     "body"
     t.integer  "visits_count"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "posts", force: true do |t|
+    t.text     "content"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "location"
+    t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
