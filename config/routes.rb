ef7ef6c5f4889c901ce0f30d7cd9a82aac5422df
spabon <sspabonee@hotmail.com>
2015-05-09 05:02:00 +0000

    Add Sin Up and Sin In

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,13 @@ Rails.application.routes.draw do
 
   resources :users
 
-  get 'welcome/index'
+  #get 'welcome/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-   root 'welcome#index'
+   root 'users#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
